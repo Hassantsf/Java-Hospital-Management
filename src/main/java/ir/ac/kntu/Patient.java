16457637e2d3c11b7263765cf6feb1e7912ed061
Hassan <hassantaheriansf@yahoo.com>
2020-03-26 21:26:07 +0430

    Add Patient Method

--- src/main/java/ir/ac/kntu/Patient.java
+++ src/main/java/ir/ac/kntu/Patient.java
@@ -1,6 +1,7 @@
 package ir.ac.kntu;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Patient {
     private String firstName = null;
@@ -15,6 +16,22 @@ public class Patient {
     private int caseNumber = 0;
     private String insurance = null;
     private String doctor = null;
+
+    public Patient(String firstName, String lastName, String section, String disease, String typeOfApply,
+                   String insurance, String doctor, int id, int age, int caseNumber, Date confineDate, boolean gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.section = section;
+        this.disease = disease;
+        this.typeOfApply = typeOfApply;
+        this.confineDate = confineDate;
+        this.gender = gender;
+        this.age = age;
+        this.caseNumber = caseNumber;
+        this.insurance = insurance;
+        this.doctor = doctor;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -111,4 +128,46 @@ public class Patient {
     public void setDoctor(String doctor) {
         this.doctor = doctor;
     }
+
+    public Patient addPatient() {
+        Scanner sc = new Scanner(System.in);
+        String[] strStore = null;
+        int[] intStore = null;
+        boolean boolStore;
+        Date dateStore = null;
+        String junk;
+        System.out.println("Enter first name: ");
+        strStore[0] = sc.nextLine();
+        System.out.println("Enter last name: ");
+        strStore[1] = sc.nextLine();
+        System.out.println("Enter section: ");
+        strStore[2] = sc.nextLine();
+        System.out.println("Enter disease: ");
+        strStore[3] = sc.nextLine();
+        System.out.println("Enter type of apply: ");
+        strStore[4] = sc.nextLine();
+        System.out.println("Enter insurance: ");
+        strStore[5] = sc.nextLine();
+        System.out.println("Enter doctor: ");
+        strStore[6] = sc.nextLine();
+
+        System.out.println("Enter National ID: ");
+        intStore[0] = sc.nextInt();
+        System.out.println("Enter Age: ");
+        intStore[1] = sc.nextInt();
+        System.out.println("Enter caseNumber: ");
+        intStore[2] = sc.nextInt();
+//        Date Section
+        System.out.println("Please enter the right number:");
+        System.out.println("1) Male 2)Female");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            boolStore = true;
+        } else {
+            boolStore = false;
+        }
+        return new Patient(strStore[0], strStore[1], strStore[2], strStore[3], strStore[4], strStore[5]
+                            strStore[6], intStore[0], intStore[1], intStore[2], dateStore, boolStore);
+    }
+
 }
